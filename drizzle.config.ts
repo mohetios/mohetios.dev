c58254242c91c86ec3787835c39323aa28ed3122
Ali Zemani <mehot1@gmail.com>
2026-06-01 20:42:24 +0330

    third bootup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,10 +4,4 @@ export default defineConfig({
   schema: './server/models/schema.ts',
   out: './drizzle/migrations',
   dialect: 'sqlite',
-  driver: 'd1-http',
-  dbCredentials: {
-    accountId: process.env.CLOUDFLARE_ACCOUNT_ID || '',
-    databaseId: process.env.CLOUDFLARE_DATABASE_ID || '',
-    token: process.env.CLOUDFLARE_D1_TOKEN || ''
-  }
 })
